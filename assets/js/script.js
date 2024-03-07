a21bcfa8b51ad2f95065a90efb3d434d35d8056a
IRCF <ircfspace@gmail.com>
2024-03-07 12:41:53 +0330

    removeNonEnglishWords

--- assets/js/script.js
+++ assets/js/script.js
@@ -337,8 +337,11 @@ function cleanUrl(url) {
     return randomizeCase(finalUrl);
 }
 
-function removeEmoji(remarks) {
-    return remarks.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '').replace(/\s+/g, ' ')
+function removeNonEnglishWords(remarks) {
+    //remarks = remarks.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
+    remarks = remarks.replace(/[^a-zA-Z0-9\s_.()\[\]]/g, '');
+    remarks = remarks.replace(/\s+/g, ' ');
+    return remarks;
 }
 
 function generateJson() {
@@ -371,7 +374,7 @@ function generateJson() {
         if ( remarks === "") {
             remarks = "IRCF Fragment";
         }
-        remarks = removeEmoji(remarks);
+        remarks = removeNonEnglishWords(remarks);
         if ( uuid === '' || sni === ''|| port === '' ) {
             alert('فرم را تکمیل نمایید.');
             return false;
